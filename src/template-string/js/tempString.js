b1146a4645cb7caa7661402052d3ea0c59108808
Anny Guttierrez <agutilop@everis.com>
2019-03-30 15:12:41 -0500

    Add landin page
    - Add landing page.

--- src/template-string/js/tempString.js
+++ src/template-string/js/tempString.js
@@ -17,12 +17,68 @@ const objTemp = {
       <img src="https://static.wixstatic.com/media/219d1c_1b65514082fe413e8bbacd3246748889~mv2.png/v1/fill/w_103,h_93,al_c,q_80,usm_0.66_1.00_0.01/219d1c_1b65514082fe413e8bbacd3246748889~mv2.webp" alt="Conejo">
     </figure>
   </div>`,
-  home: `<div class="position">
-    <h2 class="text-center">¡Bienvenido a nuestra página!</h2>
-    <figure class="text-center">
-      <img class="image" src="http://olegif.com/bin/gifs/00/39/56.gif" alt="Conejo codeando">
-    </figure>
-  </div>`,
+  home: `
+  <div id="main" class="flex">
+    <div class="flex descriptionContainer">
+      <div>
+        <p class="mainTitle">Women in STEM</p>
+      </div>
+      <div class="textContainer">
+        <p class="subtitle">Programa de mentoría en <span class="bold">ciencia</span>, <span class="bold">tecnología</span>,
+          <span class="bold">ingeniería</span> y <span class="bold">matemáticas</span>.</p>
+        <p class="subtitle">Enfocado en mujeres.</p>
+      </div>
+      <div class="flex joinusContainer">
+        <div class="joinusBtn">
+          <p class="btnColor">Únete</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex imageContainer">
+      <img class="heroImage" src="assets/girl.png" alt="girl" />
+    </div>
+  </div>
+  <div id="carousel" class="flex">
+    <img class="logoActivities" src="assets/logo-activities.png" alt="logo-activities" />
+    <div class="flex corouselTextContainer">
+      <div class="flex textContainer">
+        <p class="carouselTitle">Learn from women who change the world.</p>
+        <p class="carouselTitle">Be one of them.</p>
+        <p class="descriptionCarousel">We are an organization that seeks that more women begin to be interested and
+          learn
+          about science, technology, engineering and mathematics through the mentoring of other women who work in these
+          areas in a professional manner. We seek to bring out the best potential of the members, and demolish many
+          myths
+          about STEM.
+        </p>
+      </div>
+      <div class="flex imageContainerWoman">
+        <img class="women" src="assets/girl11.png" alt="women" />
+      </div>
+    </div>
+  </div>
+  <div id="stem" class="flex">
+      <div class="flex card">
+        <div>
+          <img src="assets/" />
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div class="flex card">
+        <p>Hello</p>
+        <p>jkhdjkhhudjkasdssdkj</p>
+      </div>
+      <div class="flex card">
+        <p>Hello</p>
+        <p>jkhdjkhhudjkasdssdkj</p>
+      </div>
+      <div class="flex card">
+        <p>Hello</p>
+        <p>jkhdjkhhudjkasdssdkj</p>
+      </div>
+  </div> `,
   lugares: `<div class="position">
     <h2 class="text-center">Lugares de adopción</h2>
     <figure>
